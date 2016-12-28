ad37e46252bc24a3b43dff0436656ae35845c10f
Johnathon Sykes <johndsykes86@gmail.com>
2016-12-27 18:12:51 -0800

    added jquery grid logic.

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,26 @@
+$(document).ready (function(){
 
+  var square = prompt("Please enter the amount of squares per row that you want");
+
+
+
+  function grid(){
+    for (i=0; i<square * square; i++){
+      $('.container').append('<div class=square></div>');
+    }
+  }
+
+  grid();
+
+  alert($('.square').length);
+
+
+  $('.square').on('mouseenter', function(){
+    $(this).addClass('color');
+  })
+
+  $('button').on('click', function(){
+    $('.square').removeClass('color');
+  })
+
+});
