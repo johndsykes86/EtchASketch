8688cf5bbd9713447d079a121210a92c7934ef74
Johnathon Sykes <johndsykes86@gmail.com>
2016-12-28 19:21:25 -0800

    Added dynamic grid logic

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,32 @@
 $(document).ready (function(){
 
-  var square = prompt("Please enter the amount of squares per row that you want");
 
 
 
   function grid(){
-    for (i=0; i<square * square; i++){
+    var square = prompt("Please enter the amount of squares per row that you want");
+    var height = parseInt($('.container').css("height"));
+    var value = height / square;
+
+
+    for (i=0; i<Math.pow(square, 2); i++){
       $('.container').append('<div class=square></div>');
     }
+
+    $('.square').css({'width': value, 'height': value});
+
+
   }
 
   grid();
-
-  alert($('.square').length);
-
 
   $('.square').on('mouseenter', function(){
     $(this).addClass('color');
   })
 
   $('button').on('click', function(){
-    $('.square').removeClass('color');
+    $('.container').empty();
+    grid();
   })
 
 });
