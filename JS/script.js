ab9304bf78c6fa232128d980946bab1cecd93295
Johnathon Sykes <johndsykes86@gmail.com>
2016-12-30 08:47:48 -0800

    modified randomcolor generator to be in grid function.

--- JS/script.js
+++ JS/script.js
@@ -3,13 +3,31 @@ $(document).ready (function(){
 
 
 
+
+
+
   function grid(){
+
+    function randomcolor(){
+      function random(){
+        return Math.floor(Math.random()*(255-0+1))+0;
+      }
+
+      r = random();
+      g = random();
+      b = random();
+      var value = "rgb("+r+","+g+","+b+")";
+      return value
+
+    }
 
     function square(){
       value = prompt("Please enter the amount of squares per row that you want. \rMin value: 1 \rMax value: 100");
       return value;
     }
     square = square();
+
+
 
     if (square < 1 || square > 100){
       alert("This is an invalid entry, please enter a number between 1 or 100.");
@@ -18,43 +36,22 @@ $(document).ready (function(){
       for (i=0; i<Math.pow(square, 2); i++){
         $('.container').append('<div class=square></div>');
       }
-      //
       var height = parseInt($('.container').css("height"));
       var value = height / square;
       $('.square').css({'width': value, 'height': value});
 
       $('.square').on('mouseenter', function(){
         $(this).addClass('color');
+        rgb = randomcolor();
+        $('.square').css('background-color', rgb)
       })
 
     }
 
   }
 
-function randomcolor(){
-  function random(){
-    return Math.floor(Math.random()*(255-0+1))+0;
-  }
-
-  r = random();
-  g = random();
-  b = random();
-  var value = "rgb("+r+","+g+","+b+")";
-  return value
 
 
-}
-
-
-console.log(randomcolor())
-
-
-$('.random').on('click', function(){
-    rgb = randomcolor()
-  $('.square').css("background-color", rgb);
-  $('body').css("background-color", rgb)
-
-})
 
   $('.reset').on('click', function(){
     $('.container').empty();
